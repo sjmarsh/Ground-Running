e55ad4aa18e1af0493c44d67e692c70eae84629d
Shannon Marsh <sjmarsh31@gmail.com>
2015-05-03 10:38:55 +1000

    Add Logging to Stash Repo Creator

--- src/StashAutomation/RepositoryCreator.cs
+++ src/StashAutomation/RepositoryCreator.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text;
 using Newtonsoft.Json;
 using StashAutomation.Models;
+using NLog;
 
 namespace StashAutomation
 {
@@ -12,29 +13,35 @@ namespace StashAutomation
     {       
         private const string ScmId = "git";
         private readonly WebClient _client;
+
+        private Logger _logger;
         
         public RepositoryCreator()
         {
             _client = new WebClient();
-        
+
+            _logger = LogManager.GetCurrentClassLogger();
         }
 
         // TODO Use oAuth instead of Basic Auth
         public void Create(string repoName, string stashProjectKey, string stashUrl, string stashBase64Credentials)
         {
-            _client.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
-            _client.Headers.Add(HttpRequestHeader.Authorization, "Basic " + stashBase64Credentials);
-
-            var stashApiBaseUrl = string.Format("http://{0}/rest/api/1.0/projects/", stashUrl);
-            var createRepoUrl = string.Format("{0}{1}/repos/", stashApiBaseUrl, stashProjectKey);
-            var repository = new Repository{ Name = repoName, ScmId = ScmId };
-
+            _logger.Info("Creating Stash Repository Named: {0} on Stash Server: {1} using Project Key: {2}", repoName, stashUrl, stashProjectKey);
+           
             try
             {
+                _client.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
+                _client.Headers.Add(HttpRequestHeader.Authorization, "Basic " + stashBase64Credentials);
+
+                var stashApiBaseUrl = string.Format("http://{0}/rest/api/1.0/projects/", stashUrl);
+                var createRepoUrl = string.Format("{0}{1}/repos/", stashApiBaseUrl, stashProjectKey);
+                var repository = new Repository { Name = repoName, ScmId = ScmId };
+
                 _client.UploadData(createRepoUrl, "POST", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(repository)));
             }
             catch (Exception ex)
             {
+                _logger.Error("Error occured creating Stash Repository", ex);
                 // TODO return encapsulated error response object so can display nice message
                 throw;
             }
@@ -42,28 +49,37 @@ namespace StashAutomation
        
         public void Publish(string repoLocation, string repoName, string stashProjectKey, string stashPublishUrl)
         {
-            AddGitIgnoresToRepo(repoLocation);
-            
-            var repoUrl = string.Format("ssh://git@{0}/{1}/{2}.git", stashPublishUrl, stashProjectKey, repoName);
+            _logger.Info("Publishing Repository from local path {0} to Stash {1}", repoLocation, stashPublishUrl);
 
-            var batchCommand = new StringBuilder();
-            batchCommand.AppendLine("cd " + repoLocation);
-            batchCommand.AppendLine("git init");
-            batchCommand.AppendLine("git add .");
-            batchCommand.AppendLine("git commit -m \"Initial Automated Commit\"");
-            batchCommand.AppendLine("git remote add origin " + repoUrl);
-            batchCommand.AppendLine("git remote -v"); // verfiy nre remote url
-            batchCommand.AppendLine("git push origin master");
-
-            var tempBatchFile = repoLocation + "\\" + "publishNewRepo.bat";
-            File.AppendAllText(tempBatchFile, batchCommand.ToString());
-
-            var process = new Process()
+            try
             {
-                StartInfo = new ProcessStartInfo(tempBatchFile)
-            };
-            process.Start();
-            process.WaitForExit();
+                AddGitIgnoresToRepo(repoLocation);
+
+                var repoUrl = string.Format("ssh://git@{0}/{1}/{2}.git", stashPublishUrl, stashProjectKey, repoName);
+
+                var batchCommand = new StringBuilder();
+                batchCommand.AppendLine("cd " + repoLocation);
+                batchCommand.AppendLine("git init");
+                batchCommand.AppendLine("git add .");
+                batchCommand.AppendLine("git commit -m \"Initial Automated Commit\"");
+                batchCommand.AppendLine("git remote add origin " + repoUrl);
+                batchCommand.AppendLine("git remote -v"); // verfiy remote url
+                batchCommand.AppendLine("git push origin master");
+
+                var tempBatchFile = repoLocation + "\\" + "publishNewRepo.bat";
+                File.AppendAllText(tempBatchFile, batchCommand.ToString());
+
+                var process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo(tempBatchFile)
+                };
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error occured Publishing Repository to Stash.", ex);
+            }
         }
 
         private void AddGitIgnoresToRepo(string repoLocation)
