68bdf08b191633e339f372b1c13dd3093c8f464d
Shannon Marsh <sjmarsh31@gmail.com>
2015-05-05 19:36:30 +1000

    Add AutomationException class

---
+++ src/GroundRunning.Common/AutomationException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroundRunning.Common
+{
+    public class AutomationException : Exception
+    {
+        private string p;
+        private decimal requiredVersion;
+
+
+        public AutomationException()
+        {
+        }
+
+        public AutomationException(string message) : base(message)
+        {
+        }
+        
+        public AutomationException(string message, Exception exception) : base(message, exception)
+        {
+        }
+
+        public AutomationException(string messageFormat, params object[] args) : base(string.Format(messageFormat, args))
+        {
+        }     
+    }
+}
